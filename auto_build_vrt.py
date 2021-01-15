0a93b99d8b1aa3cc3f7ef295eb7b8bc067569d1f
initze <ingmarnitze@gmail.com>
2021-01-15 15:27:15 +0100

    input files to textfile, fix error due to too long commendline command

--- auto_build_vrt.py
+++ auto_build_vrt.py
@@ -1,10 +1,20 @@
+
+# author: Ingmar Nitze, ingmar.nitze@awi
+# version: 0.2
+# date: 2021-01-15
+
 import glob2, os
+
 def build_vrt(string):
+    textfile = 'filelist.txt'
     flist = glob2.glob(f'**/*{string}*.tif')
-    l = ' '.join(flist)
-    print(f"Running {string}. {len(flist)} files")
-    s = f'gdalbuildvrt {string}.vrt {l}'
+    if len(flist) == 0:
+        pass
+    with open(textfile, 'w') as f:
+        f.writelines("\n".join(flist))
+    s = f'gdalbuildvrt -input_file_list {textfile} {string}.vrt'
     os.system(s)
+    os.remove(textfile)
 	
 def main():
     for s in ['rgb', 'nir', 'dsm']:
